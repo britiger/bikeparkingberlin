b0034c03fdd0df1efaf0a894e1b0a548b00fb864
Christopher Lorenz <britiger@gmx.net>
2019-08-18 21:36:53 +0200

    Adding OpenData SH Norderstedt

--- database/import_external.sh
+++ database/import_external.sh
@@ -15,6 +15,13 @@ then
     wget -O external_data/s_Fahrradstaender.gml https://fbinter.stadt-berlin.de/fb/wfs/data/senstadt/s_Fahrradstaender\?service\=WFS\&version\=1.1.0\&request\=GetFeature\&typeName\=fis:s_Fahrradstaender
 fi
 
+if [ ! -f external_data/13_Bike_und_ride.csv ]
+then
+    echo "Download Norderstedt Fahhradabstellanlagen an ÖPNV-Haltestellen"
+    wget -O external_data/13_Bike_und_ride.csv http://185.223.104.6/data/norderstedt/13_Bike_und_ride.csv
+fi
+
+# Check ogr2ogr
 if [ -z `which ogr2ogr` ]
 then
     echo "Please install ogr2ogr e.g. 'apt install gdal-bin'"
@@ -24,12 +31,18 @@ fi
 # Import data
 psql -f sql/create_external_table.sql
 echo "Importing External Data ..."
+
 # Berlin Fahrradständer
 echo "Import Berlin Fahrradständer Befahrung 2014"
 ogr2ogr -f "PostgreSQL" PG:"host=$PGHOST port=$PGPORT dbname=$PGDATABASE user=$PGUSER password=$PGPASSWORD" \
     -progress -overwrite -lco GEOMETRY_NAME=geom \
     -s_srs EPSG:25833 -t_srs EPSG:3857 \
     external_data/s_Fahrradstaender.gml
-
 # create views and entry in external_data   
 psql -f sql/create_external_berlin.sql
+
+# Norderstedt Fahrrad ÖPNV
+echo "Import Norderstedt Fahhradabstellanlagen an ÖPNV-Haltestellen"
+psql -f sql/create_external_noderstedt.sql
+cat external_data/13_Bike_und_ride.csv | psql -c "COPY all_parking_nstedt(IDENT,BEZEICH,OEPNV,ART,ANZAHL,X,Y) FROM STDIN DELIMITER ';' CSV HEADER;"
+psql -c "UPDATE all_parking_nstedt SET ogc_fid=IDENT, geom=ST_TRANSFORM(ST_SetSRID(ST_MakePoint(X, Y),32632),3857)"
---
+++ database/sql/create_external_noderstedt.sql
@@ -0,0 +1,23 @@
+DROP TABLE IF EXISTS all_parking_nstedt CASCADE;
+CREATE TABLE all_parking_nstedt (
+    ogc_fid INT,
+    IDENT INT,
+    BEZEICH VARCHAR(255),
+    OEPNV VARCHAR(255),
+    ART VARCHAR(255),
+    ANZAHL INT,
+    X INT,
+    Y INT,
+    geom geometry
+);
+
+CREATE OR REPLACE VIEW missing_parking_nstedt AS
+SELECT nstedt.*,  ST_AsGeoJSON(st_transform(nstedt.geom,4326)) as geojson
+FROM public.all_parking_nstedt nstedt
+LEFT JOIN imposm3.view_parking osm
+ON osm.geom && ST_Expand(st_transform(nstedt.geom,3857), 50)
+WHERE osm.osm_id IS NULL;
+
+DELETE FROM external_data WHERE city='Norderstedt';
+INSERT INTO external_data (city, table_all_parking, table_missing_parking, datasource, center_lat, center_lon, zoom_level) 
+    VALUES ('Norderstedt', 'all_parking_nstedt', 'missing_parking_nstedt', 'Open Data Schleswig-Holstein Fahhradabstellanlagen an ÖPNV-Haltestellen, Standort und Ausstattung Norderstedt', 53.692139, 9.995334, 13);
