0f19d41207ae5ac0cbf4a5e86a7214f9727b3942
Christopher Lorenz <britiger@gmx.net>
2019-10-11 20:31:29 +0200

    Import London 2nd Dataset currenty not used

--- database/import_external.sh
+++ database/import_external.sh
@@ -48,8 +48,8 @@ download_external "Moers Fahrradständer" http://geoportal-niederrhein.de/files/
 download_external "Bonn Fahrradstellplätze" https://stadtplan.bonn.de/geojson?Thema=24840 fahrradstellplaetze_bonn.geojson
 download_external "Wuppertal Fahrradstellplätze" https://www.offenedaten-wuppertal.de/node/1257/download Radabstellanlagen_wuppertal.zip
 download_external "Köln Fahrrad Förderung" "https://geoportal.stadt-koeln.de/arcgis/rest/services/Fahrradverkehr_Ma%C3%9Fnahmen/MapServer/0/query?where=objectid+is+not+null&text=&objectIds=&time=&geometry=&geometryType=esriGeometryEnvelope&inSR=&spatialRel=esriSpatialRelIntersects&relationParam=&outFields=*&returnGeometry=true&returnTrueCurves=false&maxAllowableOffset=&geometryPrecision=&outSR=4326&returnIdsOnly=false&returnCountOnly=false&orderByFields=&groupByFieldsForStatistics=&outStatistics=&returnZ=false&returnM=false&gdbVersion=&returnDistinctValues=false&resultOffset=&resultRecordCount=&f=pjson" fahrrad_massnahme_koeln.geojson
-download_external "London Cycling Infrastructure" https://cycling.data.tfl.gov.uk/CyclingInfrastructure/data/points/cycle_parking.json london_parking.json
-
+download_external "London Cycling Infrastructure" https://cycling.data.tfl.gov.uk/CyclingInfrastructure/data/points/cycle_parking.json london_parking.geojson
+download_external "London Cycling Infrastructure MapInfo" https://cycling.data.tfl.gov.uk/CycleParking/cycle-parking-map-info.zip london_mapinfo.zip
 # Import data
 psql -f sql/create_external_table.sql
 echo "Importing External Data ..."
@@ -143,6 +143,12 @@ ogr2ogr -f "PostgreSQL" PG:"$OGR2OGR_PGSQL" \
     -overwrite -lco GEOMETRY_NAME=geom \
     -t_srs EPSG:3857 \
     -nln all_parking_london \
-    external_data/london_parking.json
+    external_data/london_parking.geojson
 psql -f sql/create_external_london.sql
 cat sql/create_external_template.sql | sed -e 's/#CITY#/london/g' | psql
+
+ogr2ogr -f "PostgreSQL" PG:"$OGR2OGR_PGSQL" \
+    -overwrite -lco GEOMETRY_NAME=geom \
+    -t_srs EPSG:3857 \
+    -nln all_parking_london_map \
+    external_data/CycleParking\ 2015.TAB
