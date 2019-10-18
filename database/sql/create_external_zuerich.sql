759798a4b560b01ac1d7a3a09bf8e96ea5ba8225
Christopher Lorenz <britiger@gmx.net>
2019-10-18 15:26:03 +0200

    Fix Zürich remove motorbike

--- database/sql/create_external_zuerich.sql
+++ database/sql/create_external_zuerich.sql
@@ -1,10 +1,12 @@
 SET client_min_messages TO WARNING;
 
+DELETE FROM extern.all_parking_zuerich WHERE name = 'Motorrad';
+
 DELETE FROM extern.external_data WHERE city='Zürich';
 INSERT INTO extern.external_data (city, suffix, datasource, datasource_link, license, license_link, center_lat, center_lon, zoom_level, admin_osm_id)
     VALUES ('Zürich', 
         'zuerich', 
-        'Stadt Zürich - Zweiradabstellplätze in der Stadt Zürich',
+        'Stadt Zürich - Zweiradabstellplätze in der Stadt Zürich (ohne Datensätze nur Motorrad)',
         'https://data.stadt-zuerich.ch/dataset/zweiradabstellplatz',
         'Creative Commons CCZero',
         'http://www.opendefinition.org/licenses/cc-zero',
